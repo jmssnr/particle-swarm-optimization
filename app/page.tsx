636baef51e1e700f0e3672fbc76cfe5f1930e795
jmssnr <maussner.johannes@gmail.com>
2025-05-08 22:08:49 +0200

    links hyperparameters to optimizer

--- app/page.tsx
+++ app/page.tsx
@@ -7,26 +7,49 @@ import { Slider } from "@/components/ui/slider";
 import { minimize } from "@/core/minimize";
 import { objective } from "@/core/objective";
 import { ParentSize } from "@visx/responsive";
+import { useState } from "react";
 
 export default function Home() {
-  const result = minimize();
-  console.log(result.map((r) => r.particleTrajectories));
+  const [inertiaWeight, setIntertiaWeight] = useState(0.1);
+  const [cognitiveWeight, setCognitiveWeight] = useState(0.25);
+  const [socialWeight, setSocialWeight] = useState(1);
+
+  const result = minimize(inertiaWeight, cognitiveWeight, socialWeight);
+
   return (
     <main className="w-screen h-screen flex gap-2 p-2">
       <section className="flex-1 flex flex-col gap-2">
         <Card>
           <CardContent className="flex justify-evenly gap-6 p-4">
             <div className="flex flex-col gap-2 flex-1">
-              <Label>Parameter 1</Label>
-              <Slider />
+              <Label>Inertia Weight</Label>
+              <Slider
+                min={0}
+                max={1}
+                step={0.01}
+                value={[inertiaWeight]}
+                onValueChange={(v) => setIntertiaWeight(v[0])}
+              />
             </div>
             <div className="flex flex-col gap-2 flex-1">
-              <Label>Parameter 2</Label>
-              <Slider />
+              <Label>Cognitive Weight</Label>
+              <Slider
+                min={0}
+                max={1}
+                step={0.01}
+                value={[cognitiveWeight]}
+                onValueChange={(v) => setCognitiveWeight(v[0])}
+              />
             </div>
             <div className="flex flex-col gap-2 flex-1">
-              <Label>Parameter 3</Label>
-              <Slider />
+              <Label>Social Weight</Label>
+              <Slider
+                min={0}
+                max={1}
+                step={0.01}
+                value={[socialWeight]}
+                onValueChange={(v) => setSocialWeight(v[0])}
+              />
             </div>
           </CardContent>
         </Card>
